93a28f2f0a5ed7afe8ceec4ac3ff8384cb9f7eda
José Matela <jmatelaneto@gmail.com>
2022-06-30 18:36:00 -0300

    creating a folder for the Dicee App

---
+++ dicee-app/dicee-app/dicee_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_appApp.swift
+//  dicee-app
+//
+//  Created by José Matela Neto on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct dicee_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
